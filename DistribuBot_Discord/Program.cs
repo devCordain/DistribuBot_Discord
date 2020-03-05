fa8f254f3b5e89edbd4a5d4c9000e685ab148b94
devCordain <pctkar@gmail.com>
2020-03-05 12:23:33 +0100

    Added Discord.Net websocket.

--- DistribuBot_Discord/Program.cs
+++ DistribuBot_Discord/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace DistribuBot_Discord
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	public class Program
+	{
+		private DiscordSocketClient client;
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
 
-        }
-    }
+		public async Task MainAsync()
+		{
+			client = new DiscordSocketClient();
+
+			client.Log += Log;
+
+			var token = File.ReadAllText("botToken.txt");
+
+			await client.LoginAsync(TokenType.Bot, token);
+			await client.StartAsync();
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
 }
